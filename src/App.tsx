12de2685db07f91d72e8d66386df5182dc2dd6e8
Animalfox <a72e@outlook.com>
2024-06-02 15:54:49 +0300

    Add Main Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Main />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
