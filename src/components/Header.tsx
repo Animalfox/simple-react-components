d01318b95861ee6348c1a68982a96244cc312ef6
Animalfox <a72e@outlook.com>
2024-06-02 15:48:27 +0300

    Update Header Component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 export default function Header() {
   return (
     <header>
-      <h1>Header Title</h1>
-      <p>Header description</p>
+      <h1>Simple React Components</h1>
+      <p>
+        Basic React project demonstrating the creation and use of components.
+      </p>
     </header>
   );
 }
